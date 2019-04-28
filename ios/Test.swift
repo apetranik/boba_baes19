20cab5d7e6997b23a6eb32061406c9e60a6dcaf9
Aliya Petranik <petranik@usc.edu>
2019-04-28 13:08:26 -0700

    in transport

---
+++ ios/Test.swift
@@ -0,0 +1,9 @@
+//
+//  Test.swift
+//  boba-baes
+//
+//  Created by Aliya Petranik on 4/26/19.
+//  Copyright © 2019 650 Industries, Inc. All rights reserved.
+//
+
+import Foundation
--- src/native/components/About.js
+++ src/native/components/About.js
@@ -3,6 +3,7 @@ import {
   View, Image, Text, TouchableOpacity,
 } from 'react-native';
 
+import Bluetooth from 'Bluetooth.js';
 import twenty from '../../../docs/bobacup25.png';
 import twentyselected from '../../../docs/bobacup25-selected.png';
 
@@ -26,7 +27,6 @@ class About extends React.Component {
     sweetness: '',
     firmness: '',
     inProgress: false,
-
   };
 
   // Update sweetness onPress
@@ -40,12 +40,11 @@ class About extends React.Component {
 
   start = () => {
     this.setState({ inProgress: true });
-
-  }
+  };
 
   back = () => {
     this.setState({ inProgress: false });
-  }
+  };
 
   render() {
     const { sweetness, firmness } = this.state;
@@ -54,68 +53,57 @@ class About extends React.Component {
         <View>
           <TouchableOpacity>
             <Text onPress={() => this.back()}> Back</Text>
-
           </TouchableOpacity>
-
         </View>
       );
     }
     return (
       <View style={styles.main}>
+        <Bluetooth />
         <Text style={styles.subtitle}>Select your sweetness</Text>
 
         <View style={styles.sweetness}>
           <TouchableOpacity onPress={() => this.selectSweetness('25')}>
-            <Image
-              style={styles.img}
-              source={sweetness === '25' ? twentyselected : twenty}
-            />
+            <Image style={styles.img} source={sweetness === '25' ? twentyselected : twenty} />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.selectSweetness('50')}>
-            <Image
-              style={styles.img}
-              source={sweetness === '50' ? fiftyselectd : fifty}
-            />
+            <Image style={styles.img} source={sweetness === '50' ? fiftyselectd : fifty} />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.selectSweetness('75')}>
-            <Image
-              style={styles.img}
-              source={sweetness === '75' ? seventyselected : seventy}
-            />
+            <Image style={styles.img} source={sweetness === '75' ? seventyselected : seventy} />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this.selectSweetness('100')}>
-            <Image
-              style={styles.img}
-              source={sweetness === '100' ? hundredselected : hundred}
-            />
+            <Image style={styles.img} source={sweetness === '100' ? hundredselected : hundred} />
           </TouchableOpacity>
         </View>
         <Text style={styles.subtitle}>Select your firmness</Text>
 
         <View style={styles.bobaAmount}>
-          <TouchableOpacity onPress={() => this.selectFirmness('0')} style={firmness === '0' ? styles.selected_touch : styles.non_selected_touch}>
-
+          <TouchableOpacity
+            onPress={() => this.selectFirmness('0')}
+            style={firmness === '0' ? styles.selected_touch : styles.non_selected_touch}
+          >
             <Text style={styles.textOptions}> Soft </Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => this.selectFirmness('50')} style={firmness === '50' ? styles.selected_touch : styles.non_selected_touch}>
-
+          <TouchableOpacity
+            onPress={() => this.selectFirmness('50')}
+            style={firmness === '50' ? styles.selected_touch : styles.non_selected_touch}
+          >
             <Text style={styles.textOptions}> Normal </Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => this.selectFirmness('100')} style={firmness === '100' ? styles.selected_touch : styles.non_selected_touch}>
-
+          <TouchableOpacity
+            onPress={() => this.selectFirmness('100')}
+            style={firmness === '100' ? styles.selected_touch : styles.non_selected_touch}
+          >
             <Text style={styles.textOptions}> Firm </Text>
           </TouchableOpacity>
-
         </View>
-
 
         <TouchableOpacity onPress={() => this.start()} style={styles.start_button}>
           <Text style={styles.startText}> START BOBA </Text>
         </TouchableOpacity>
-
-
       </View>
     );
   }
